542fe4f5ef2c8c3d5378919c24813e9d28c21f3d
FoVlaX <fovlaxcorp@gmail.com>
2020-12-17 16:48:20 +0300

    test _ 2

--- app/src/test/java/com/example/pagerlistapp/RepositoryContext.kt
+++ app/src/test/java/com/example/pagerlistapp/RepositoryContext.kt
@@ -1,14 +1,19 @@
 package com.example.pagerlistapp
 
 import android.content.Context
+import androidx.lifecycle.MutableLiveData
 import com.example.pagerlistapp.models.Work
 import com.example.pagerlistapp.repository.Repository
-import org.robolectric.annotation.Implementation
+import com.example.pagerlistapp.repository.State
 import org.robolectric.annotation.Implements
-import org.robolectric.shadows.ShadowContextImpl
+import org.robolectric.annotation.RealObject
+
 
 @Implements(Repository::class)
 class RepositoryContext {
+
+    private val currentWorksState: MutableLiveData<State> = MutableLiveData(State.Loaded())
+
     fun getWorks(offset: Int, count: Int): List<Work?>?{
         return List(count){
             val work = Work()
@@ -17,4 +22,9 @@ class RepositoryContext {
             work
         }
     }
+
+    fun <T> getWorksState(): T {
+        return  currentWorksState as T
+    }
+
 }
